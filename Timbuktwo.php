8b18599d8cefaec03c5518f9f9f3778208ac2133
Lance Cameron <vishtany@gmail.com>
2016-12-16 21:28:42 -0500

    create first version and more
    created version version and added preliminary timbuktwo class along with its first preliminary function layout.

---
+++ Timbuktwo.php
@@ -0,0 +1,20 @@
+<?php
+
+# namespace Timbuktwo; // consider the use of a namespace for later
+
+
+class Timbuktwo {
+    /**
+     * Convert to/from/between Bitcoin units
+     *
+     * @param string $amount a amount needing conversion
+     * @param array $type satoshi|mbit|bitcoin/btc
+     *
+     * @return string containing new unit value
+     * STATUS:NOT_COMPLETED
+     */
+    public static function btcUnit($amount, $type = 'satoshi') {
+        return $amount;
+    }
+}
+?>
