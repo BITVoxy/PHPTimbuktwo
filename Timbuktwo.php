fafd2a89614dfbbc537363b4395c38c5fedc7084
Lance Cameron <vishtany@gmail.com>
2016-12-16 23:12:13 -0500

    Update Timbuktwo.php

--- Timbuktwo.php
+++ Timbuktwo.php
@@ -7,7 +7,7 @@ class Timbuktwo {
      * Convert to/from/between Bitcoin units
      *
      * @param string $amount a amount needing conversion
-     * @param array $type satoshi|mbit|bitcoin/btc
+     * @param array $type satoshi|mbit/millibitcoins|bitcoin/btc|bits/uBTC
      *
      * @return string containing new unit value 
      * STATUS:NOT_COMPLETED
